ab058f85aee5ca47d0ba0b53b742a51e436f4307
DAYH228 <margo.riva@mail.ru>
2023-12-11 16:59:54 +0300

    отредактировала сайт. не знаю, работает он или нет. пофикшу потом

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ alert("ну привет")
 
 let name = prompt('Пожалуйста, представьтесь');
 alert(name + ", добро пожаловать на сайт!");
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+document.write(datetime)
